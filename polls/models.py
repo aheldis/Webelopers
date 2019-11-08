8283aa65c84b84f6e986bf25e718896f684a94b7
haniyeh <ehsanihani30@yahoo.com>
2019-11-08 11:22:46 +0330

    errrors

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=10)
+    student_id = models.IntegerField(verbose_name='شماره دانشجویی')
+    is_student = models.BooleanField()
+
+
+class Person(models.Model):
+    national_id = models.IntegerField()
+    age = models.IntegerField()
--- webelopers/urls.py
+++ webelopers/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 from django.urls import path
 
+from django.contrib import admin
 from polls.views import html_start, signup, my_view
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', html_start, name='html_start'),
     url('signup', signup, name='signup'),
     url('login', my_view, name='login')
